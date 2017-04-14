532729462a64ea70a659346dbd3729225e1384b9
magic-chenyang <15188785339@163.com>
2017-04-14 17:13:00 +0800

    Update dockerce-install.sh

--- dockerce-install.sh
+++ dockerce-install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-sudo apt-get remove docker docker-engine
-sudo apt-get update
-sudo apt-get install \
+sudo su -
+apt-get remove docker docker-engine
+apt-get update
+apt-get install \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
-sudo apt-get install \
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce
-sudo docker run hello-world
+apt-get install docker-ce
+docker run hello-world
 
 #wget https://download.docker.com/linux/ubuntu/dists/trusty/pool/stable/amd64/docker-ce_17.03.1~ce-0~ubuntu-trusty_amd64.deb
 #sudo dpkg -i docker-ce_17.03.1~ce-0~ubuntu-trusty_amd64.deb
