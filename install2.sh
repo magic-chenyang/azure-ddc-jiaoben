d638ede35ae009910b661a6e4e2d7c1b9032cd8a
magic-chenyang <15188785339@163.com>
2017-04-17 10:16:13 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://download.docker.com/linux/ubuntu/dists/trusty/pool/stable/amd64/docker-ce_17.03.1~ce-0~ubuntu-trusty_amd64.deb
 sudo dpkg -i docker-ce_17.03.1~ce-0~ubuntu-trusty_amd64.deb
+docker -v
