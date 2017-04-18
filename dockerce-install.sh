9003792b009134b417024833fa10c0d82382c085
magic-chenyang <15188785339@163.com>
2017-04-18 16:18:44 +0800

    Update dockerce-install.sh

--- dockerce-install.sh
+++ dockerce-install.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-apt-get purge docker-ce -y 
+sudo apt-get purge docker-ce -y 
 sudo rm -rf /var/lib/docker
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
-apt-get install -y \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
-apt-get install -y docker-ce
-echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=https://24z731hs.mirror.aliyuncs.com\"" | sudo tee -a /etc/default/docker
-service docker restart
-docker run hello-world
-docker -v
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=https://24z731hs.mirror.aliyuncs.com\"" | sudo tee -a /etc/default/docker
+sudo service docker restart
+sudo docker run hello-world
+sudo docker -v
+sudo expect <<EOF
+set timeout 300
+spawn docker run --rm -it --name ucp   -v /var/run/docker.sock:/var/run/docker.sock   docker/ucp:2.1.2 install  --host-address 10.0.2.8 --admin-username hydsoft --admin-password hyddocker --interactive
+expect "Additional aliases:"
+send "\n"
+expect eof
+EOF
 
 i=`hostname`|awk -F 0 `{print$2}`
 a=$(ifconfig eth0 | grep "inet addr" | awk '{ print $2}' | awk -F: '{print $2}')
@@ -30,7 +37,7 @@ do
 	if [ i -eq 1 ];
 	then
 		
-		docker run --rm -it --name ucp \
+		sudo docker run --rm -it --name ucp \
   		-v /var/run/docker.sock:/var/run/docker.sock \
   		docker/ucp:2.1.2 install \
   		--debug \
@@ -45,7 +52,7 @@ do
 		sudo apt-get install -y nfs-kernel-server
 		sudo docker swarm join-token worker|awk 'NR>2{print$0}' > /opt/worker.sh
 		sudo docker swarm join-token manager|awk 'NR>2{print$0}' > /opt/manager.sh
-		tee /etc/exports <<-'EOF' 
+		sudo tee /etc/exports <<-'EOF' 
 		/opt/ *(rw,sync,no_root_squash,no_subtree_check)
 EOF
 		sudo rpc.mountd
@@ -56,18 +63,18 @@ EOF
 		if [ i -ge 2 ];
 		then
 		sudo apt-get install -y nfs-common
-		mount -t nfs DDC-01:/opt /opt 
-		bash /opt/manager.sh
+		sudo mount -t nfs DDC-01:/opt /opt 
+		sudo bash /opt/manager.sh
 		fi
 	elif [ i -ge 4 ];
 	then
 		if [ i -le 6 ];
 		then
 		sudo apt-get install nfs-common
-		mount -t nfs DDC-01:/opt /opt 
-		bash /opt/worker.sh
+		sudo mount -t nfs DDC-01:/opt /opt 
+		sudo bash /opt/worker.sh
 #################################
-		docker run -it --rm docker/dtr install \
+		sudo docker run -it --rm docker/dtr install \
   		--dtr-external-url https://$a \
   		--ucp-node $hostname \
   		--ucp-username $ucp_admin_username \
